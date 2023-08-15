32bd754f30985c06c87d7f4de89ca1163b51f007
Ella6882 W <ella.wherritt@agilebits.com>
2023-08-15 19:24:00 +0000

    add for project 3

--- macros/assert_sub_plan_amount_is_greater_than_0.sql
+++ macros/assert_sub_plan_amount_is_greater_than_0.sql
@@ -1,4 +1,5 @@
 {% test column_is_positive(model, column_name) %}
+
 SELECT *
 FROM {{ model }}
 WHERE {{ column_name }} <= 0
--- models/marts/finance/rpt_mrr.sql
+++ models/marts/finance/rpt_mrr.sql
@@ -1,4 +1,6 @@
 -- This model is created following the dbt MRR playbook: https://www.getdbt.com/blog/modeling-subscription-revenue/
+{% set subscriptions = unit_testing_select_table(ref('dim_subscriptions'), ref('unit_test_input_dim_subscriptions')) %}
+{% set dates = unit_testing_select_table(ref('dim_dates'), ref('unit_test_input_dim_dates')) %}
 
 WITH
 
@@ -15,7 +17,7 @@ monthly_subscriptions AS (
         {{ truncate_to_date('starts_at') }} AS start_month,
         {{ truncate_to_date('ends_at') }} AS end_month
     FROM
-        {{ ref('dim_subscriptions') }}
+        {{ subscriptions }}
     WHERE
         billing_period = 'monthly'
 ),
@@ -25,7 +27,7 @@ months AS (
     SELECT
         calendar_date AS date_month
     FROM
-        {{ ref('dim_dates') }}
+        {{ dates }}
     WHERE
         day_of_month = 1
 ),
